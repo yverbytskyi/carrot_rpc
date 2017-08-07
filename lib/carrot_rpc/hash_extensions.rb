49f7811083f137b84986f5547fc665c886c5a472
Luke Imhoff <limhoff@csd.org>
2017-08-07 13:38:42 -0500

    Only rename keys in values that are Arrays or Hashes.

--- lib/carrot_rpc/hash_extensions.rb
+++ lib/carrot_rpc/hash_extensions.rb
@@ -1,17 +1,34 @@
 # Refine the Hash class with new methods and functionality.
 module CarrotRpc::HashExtensions
+  refine Array do
+    # Utility method to rename keys in a hash nested under array
+    #
+    # @param [String] find the text to look for in a keys
+    # @param [String] replace the text to replace the found text
+    # @return [Array] a new Array
+    def rename_keys(find, replace)
+      map { |element|
+        case element
+        when Array, Hash
+          element.rename_keys(find, replace)
+        else
+          element
+        end
+      }
+    end
+  end
+
   refine Hash do
     # Utility method to rename keys in a hash
     # @param [String] find the text to look for in a keys
     # @param [String] replace the text to replace the found text
     # @return [Hash] a new hash
-    def rename_keys(find, replace, new_hash = {}) # rubocop:disable Metrics/MethodLength
+    def rename_keys(find, replace, new_hash = {})
       each do |k, v|
         new_key = k.to_s.gsub(find, replace)
 
-        new_hash[new_key] = if v.is_a? Array
-                              v.map { |t| t.rename_keys(find, replace) }
-                            elsif v.is_a? Hash
+        new_hash[new_key] = case v
+                            when Array, Hash
                               v.rename_keys(find, replace)
                             else
                               v
@@ -19,6 +36,6 @@ module CarrotRpc::HashExtensions
       end
 
       new_hash
-    end # rubocop:enable Metrics/MethodLength
+    end
   end
 end
